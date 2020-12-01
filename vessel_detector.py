fab958a29bbfd15d86c8e266b6c72103b9b48977
Richard Correro <rcorrero@stanford.edu>
2020-12-01 13:55:05 -0600

    Fix misspelled key

--- vessel_detector.py
+++ vessel_detector.py
@@ -205,7 +205,7 @@ class Resize:
         
     def __call__(self, image, target) -> Tuple[torch.tensor, dict]:
         image = resize(image, size=self.output_shape, interpolation=self.interpolation)
-        target['mask'] = resize(target['mask'], size=self.output_shape,
+        target['masks'] = resize(target['masks'], size=self.output_shape,
                                 interpolation=self.interpolation)
         target['boxes'] = self.resize_boxes(target['boxes'])
         return image, target
