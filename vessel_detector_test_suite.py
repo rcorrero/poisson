00ab8421ddd3a562f0b1e9211fbb63db9ee86e64
Richard Correro <rcorrero@stanford.edu>
2020-12-03 19:56:36 -0600

    Update object detection files

--- vessel_detector_test_suite.py
+++ vessel_detector_test_suite.py
@@ -1,18 +1,21 @@
 import os
 import time
 import torch
+import math
 import numpy as np
 import pandas as pd
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
 
+from torch import tensor
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.models.detection.rpn import AnchorGenerator
 from torchvision.models.detection.faster_rcnn import FasterRCNN, FastRCNNPredictor
+from torchvision.ops.boxes import box_iou
 from torchvision.transforms import ToTensor, Compose, RandomHorizontalFlip,\
     RandomVerticalFlip,  Normalize
 from sklearn.model_selection import train_test_split
@@ -21,11 +24,7 @@ from PIL import Image, ImageFile, ImageFilter
 import pathlib
 from typing import Callable, Iterator, Union, Optional, List, Tuple, Dict
 from torchvision.transforms.functional import resize
-
-import pycocotools
-from coco_utils import *
-from coco_eval import *
-
+from torchvision.transforms.functional_tensor import resize as T_resize
 
 
 def rle2bbox(rle, shape):
@@ -71,21 +70,21 @@ def rle2bbox(rle, shape):
     return x0, y0, x1, y1
 
 
-# From: https://www.kaggle.com/paulorzp/run-length-encode-and-decode
-def rle_decode(mask_rle, shape=(299, 768)):
-    '''
-    mask_rle: run-length as string formated (start length)
-    shape: (height,width) of array to return 
-    Returns numpy array, 1 - mask, 0 - background
-    '''
-    s = mask_rle.split()
-    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
-    starts -= 1
-    ends = starts + lengths
-    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
-    for lo, hi in zip(starts, ends):
-        img[lo:hi] = 1
-    return img.reshape(shape).T  # Needed to align to RLE direction
+# # From: https://www.kaggle.com/paulorzp/run-length-encode-and-decode
+# def rle_decode(mask_rle, shape=(768, 768)):
+#     '''
+#     mask_rle: run-length as string formated (start length)
+#     shape: (height,width) of array to return 
+#     Returns numpy array, 1 - mask, 0 - background
+#     '''
+#     s = mask_rle.split()
+#     starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+#     starts -= 1
+#     ends = starts + lengths
+#     img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+#     for lo, hi in zip(starts, ends):
+#         img[lo:hi] = 1
+#     return img.reshape(shape).T  # Needed to align to RLE direction
 
 
 def is_valid(rle, shape=(768,768)):
@@ -102,19 +101,34 @@ def make_target(in_mask_list, N, shape=(768, 768)):
         target = {}
         target["boxes"] = torch.zeros((0, 4), dtype=torch.float32)
         target["labels"] = torch.zeros((0), dtype=torch.int64)
+        # target["masks"] = torch.from_numpy(np.zeros((1,
+        #                                             null_mask_shape[0],
+        #                                             null_mask_shape[1]),
+        #                                             dtype=np.uint8)) 
+        # target["area"] = torch.zeros((0,), dtype=torch.int64)
+        # target["iscrowd"] = torch.zeros((0,), dtype=torch.int64)
         return target
     bbox_array = np.zeros((N, 4), dtype=np.float32)
+    #masks = np.zeros((N, shape[0], shape[1]), dtype=np.uint8)
     labels = torch.ones((N,), dtype=torch.int64)
     i = 0
     for rle in in_mask_list:
-#        if isinstance(rle, str):
-        # bbox = tuple(x1, y1, x2, y2)
-        bbox = rle2bbox(rle, shape)
-        bbox_array[i,:] = bbox
-        i += 1
+        if isinstance(rle, str):
+            # bbox = tuple(x1, y1, x2, y2)
+            bbox = rle2bbox(rle, shape)
+            bbox_array[i,:] = bbox
+            # mask = rle_decode(rle)
+            # masks[i, :, :] = mask
+            i += 1
+    # areas = (bbox_array[:, 3] - bbox_array[:, 1]) * (bbox_array[:, 2] - bbox_array[:, 0])
+    # suppose all instances are not crowd
+    # is_crowd = torch.zeros((N,), dtype=torch.int64)
     target = {
         'boxes': torch.from_numpy(bbox_array),
         'labels': labels,
+        # #'masks': torch.from_numpy(masks),
+        # 'area': torch.from_numpy(areas),
+        # 'iscrowd': is_crowd
     }
     return target
 
@@ -157,16 +171,16 @@ def filter_masks(masks: pd.DataFrame) -> Tuple[dict, dict]:
     return image_names, image_masks
         
 
-def get_train_valid_dfs(masks: dict, seed: int = 0) -> Tuple[list, list, list, list]:
+def get_train_valid_dfs(masks: dict, seed: int = 0) -> Tuple[list, dict, list, dict]:
     ids = np.array(list(masks.keys())).reshape((len(masks),1))
     train_ids, valid_ids = train_test_split(
          ids, 
-         test_size = 0.01, 
+         test_size = 4, 
          random_state=seed
         )
     train_ids, valid_ids = list(train_ids.flatten()), list(valid_ids.flatten())
-    train_masks = [masks[idx] for idx in train_ids]
-    valid_masks = [masks[idx] for idx in valid_ids]
+    train_masks = {idx: masks[idx] for idx in train_ids}
+    valid_masks = {idx: masks[idx] for idx in valid_ids}
     return train_ids, train_masks, valid_ids, valid_masks
 
 
@@ -194,6 +208,9 @@ class Resize:
         
     def __call__(self, image, target) -> Tuple[torch.tensor, dict]:
         image = resize(image, size=self.output_shape, interpolation=self.interpolation)
+       # if target['masks'].shape[-1] != self.output_shape[-1]:
+       #     target['masks'] = T_resize(target['masks'], size=self.output_shape,
+       #                             interpolation=self.interpolation)
         target['boxes'] = self.resize_boxes(target['boxes'])
         return image, target
     
@@ -276,8 +293,8 @@ class VesselDataset(Dataset):
                                  output_shape = (299, 299)
                                 )(img, target)
             # Make image_id
-            image_id = torch.tensor([idx])
-            target["image_id"] = image_id
+            # image_id = torch.tensor([idx])
+            # target["image_id"] = image_id
         
         if self.mode =='train':
             img = self.train_transform(img)
@@ -291,8 +308,8 @@ class VesselDataset(Dataset):
         
 
 # Adapted from https://discuss.pytorch.org/t/faster-rcnn-with-inceptionv3-backbone-very-slow/91455
-def make_model(state_dict=None, num_classes=2):
-        inception = torchvision.models.inception_v3(pretrained=False, progress=False, 
+def make_model(backbone_state_dict, num_classes):
+        inception = torchvision.models.inception_v3(pretrained=True, progress=False, 
                                                     num_classes=num_classes, aux_logits=False)
         #inception.load_state_dict(torch.load(state_dict))
         modules = list(inception.children())[:-1]
@@ -313,7 +330,7 @@ def make_model(state_dict=None, num_classes=2):
         return model
 
 
-def train_print(running_loss, 
+def train_print(i, running_loss, 
                 print_every, 
                 batch_size, 
                 epoch, 
@@ -331,7 +348,8 @@ def train_one_epoch(model,
                     data_loader, 
                     device, 
                     epoch, 
-                    lr_scheduler = None, 
+                    lr_scheduler = None,
+                    batch_size = 64,
                     print_every = 100,
                     num_epochs = 30):
     model.train()
@@ -340,10 +358,11 @@ def train_one_epoch(model,
 
     for i, (inputs, targets) in enumerate(data_loader):
         start = time.time()
-        inputs = Variable(inputs).cuda()
-        targets = [{k: Variable(v).cuda() for k, v in t.items()} for t in targets]
+        inputs = [Variable(input).to(device) for input in inputs]
+        targets = [{k: Variable(v).to(device) for k, v in t.items()} for t in targets]
 
         loss_dict = model(inputs, targets)
+        print('LOSS DICT: ', loss_dict)
         losses = sum(loss for loss in loss_dict.values())
         if not math.isfinite(losses):
             print("Loss is %-10.5f, stopping training".format(losses))
@@ -356,70 +375,191 @@ def train_one_epoch(model,
         if lr_scheduler is not None:
             lr_scheduler.step()
 
-        running_loss += loss.item()
+        running_loss += losses
         end = time.time()
         minibatch_time += float(end - start)
         if (i + 1) % print_every == 0:
             minibatch_time = minibatch_time / (3600.0 * print_every)
             num_minibatches_left = 1.01 * len(data_loader) - (i + 1)
             num_minibatches_per_epoch = 1.01 * len(data_loader) - 1 + \
-            ((len(dataloader.dataset) % batch_size) / batch_size)
+            ((len(data_loader.dataset) % batch_size) / batch_size)
             num_epochs_left = num_epochs - (epoch + 1)
             time_left = minibatch_time * \
                 (num_minibatches_left + num_epochs_left * num_minibatches_per_epoch)
             time_left *= 6.0 # Adjust for timing discrepencies
-            train_print(running_loss, print_every, batch_size, epoch, 
+            train_print(i, running_loss, print_every, batch_size, epoch, 
                         num_minibatches_per_epoch, time_left)
             running_loss = 0.0
             minibatch_time = 0.0
     return model
 
 
+def align_coordinates(boxes):
+    """Align coordinates (x1,y1) < (x2,y2) to work with torchvision `box_iou` op
+    Arguments:
+        boxes (Tensor[N,4])
+    
+    Returns:
+        boxes (Tensor[N,4]): aligned box coordinates
+    """
+    x1y1 = torch.min(boxes[:,:2,],boxes[:, 2:])
+    x2y2 = torch.max(boxes[:,:2,],boxes[:, 2:])
+    boxes = torch.cat([x1y1,x2y2],dim=1)
+    return boxes
+
+
+def calculate_iou(gt, pr, form='pascal_voc'):
+    """Calculates the Intersection over Union.
+
+    Arguments:
+        gt: (torch.Tensor[N,4]) coordinates of the ground-truth boxes
+        pr: (torch.Tensor[M,4]) coordinates of the prdicted boxes
+        form: (str) gt/pred coordinates format
+            - pascal_voc: [xmin, ymin, xmax, ymax]
+            - coco: [xmin, ymin, w, h]
+    Returns:
+        iou (Tensor[N, M]): the NxM matrix containing the pairwise
+        IoU values for every element in boxes1 and boxes2
+    """
+    if form == 'coco':
+        gt = gt.clone()
+        pr = pr.clone()
+
+        gt[:,2] = gt[:,0] + gt[:,2]
+        gt[:,3] = gt[:,1] + gt[:,3]
+        pr[:,2] = pr[:,0] + pr[:,2]
+        pr[:,3] = pr[:,1] + pr[:,3]
+
+    gt = align_coordinates(gt)
+    pr = align_coordinates(pr)
+    
+    return box_iou(gt,pr)
+
+
+def get_mappings(iou_mat):
+    mappings = torch.zeros_like(iou_mat)
+    gt_count, pr_count = iou_mat.shape
+    
+    #first mapping (max iou for first pred_box)
+    if not iou_mat[:,0].eq(0.).all():
+        # if not a zero column
+        mappings[iou_mat[:,0].argsort()[-1],0] = 1
+
+    for pr_idx in range(1,pr_count):
+        # Sum of all the previous mapping columns will let 
+        # us know which gt-boxes are already assigned
+        not_assigned = torch.logical_not(mappings[:,:pr_idx].sum(1)).long()
+
+        # Considering unassigned gt-boxes for further evaluation 
+        targets = not_assigned * iou_mat[:,pr_idx]
+
+        # If no gt-box satisfy the previous conditions
+        # for the current pred-box, ignore it (False Positive)
+        if targets.eq(0).all():
+            continue
+
+        # max-iou from current column after all the filtering
+        # will be the pivot element for mapping
+        pivot = targets.argsort()[-1]
+        mappings[pivot,pr_idx] = 1
+    return mappings
+
+
+def calculate_map(gt_boxes,pr_boxes,scores,thresh=0.5,form='pascal_voc'):
+    # print("GT SHAPE: ", gt_boxes.shape)
+    # print("PR SHAPE: ", pr_boxes.shape)
+    if gt_boxes.shape[0] == 0:
+        if pr_boxes.shape[0] == 0:
+            return 1.0
+        return 0.0
+    if pr_boxes.shape[0] == 0:
+        return 0.0
+    # sorting
+    pr_boxes = pr_boxes[scores.argsort().flip(-1)]
+    iou_mat = calculate_iou(gt_boxes,pr_boxes,form) 
+    
+    # thresholding
+    iou_mat = iou_mat.where(iou_mat>thresh,tensor(0.))
+    # print('IOU SHAPE: ', iou_mat.shape)
+    
+    mappings = get_mappings(iou_mat)
+    
+    # mAP calculation
+    tp = mappings.sum()
+    fp = mappings.sum(0).eq(0).sum()
+    fn = mappings.sum(1).eq(0).sum()
+    mAP = tp / (tp+fp+fn)
+    
+    return mAP
+
+
 @torch.no_grad()
-def evaluate(model, data_loader, device):
-    n_threads = torch.get_num_threads()
-    torch.set_num_threads(n_threads)
+def evaluate(model, data_loader, device, thresh_list = [0.0, 0.25, 0.5, 0.75, 1.0]):
+    # n_threads = torch.get_num_threads()
+    # torch.set_num_threads(n_threads)
     cpu_device = torch.device("cpu")
     model.eval()
 
-    coco = get_coco_api_from_dataset(data_loader.dataset)
-    iou_types = _get_iou_types(model)
-    coco_evaluator = CocoEvaluator(coco, iou_types)
-
-    for images, targets in metric_logger.log_every(data_loader, 100, header):
+    #coco = get_coco_api_from_dataset(data_loader.dataset)
+    #iou_types = _get_iou_types(model)
+    #coco_evaluator = CocoEvaluator(coco, iou_types)
+    start = time.time()
+    mAP_dict = {thresh: [] for thresh in thresh_list}
+    for images, targets in data_loader:
         images = list(Variable(img).to(device) for img in images)
+        # print('Targets: ', targets)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-        model_time = time.time()
-        outputs = model(images)
-
+        #        model_time = time.time()
+        outputs = model(images, targets)
         outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
-        model_time = time.time() - model_time
-
-        res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
-        evaluator_time = time.time()
-        coco_evaluator.update(res)
-        evaluator_time = time.time() - evaluator_time
-        metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
+        # model_time = time.time() - model_time
+        # res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
+        # Calculate mAP
+        for thresh in thresh_list:
+            mAP_list = [calculate_map(target['boxes'], 
+                                      output['boxes'], 
+                                      output['scores'], 
+                                      thresh=thresh) \
+                        for target, output in zip(targets, outputs)]
+            #mAP = np.mean(mAP_list)
+            mAP_dict[thresh] += mAP_list # Creates a list of mAP's for each sample
+    #n_samples = len(data_loader.dataset)
+    end = time.time()
+    for thresh in thresh_list:
+        mAP_dict[thresh] = np.mean(mAP_dict[thresh])
+    # Create metrics dict
+    metrics = mAP_dict
+    metrics['eval_time'] = end - start
+        
 
     # gather the stats from all processes
-    metric_logger.synchronize_between_processes()
-    print("Averaged stats:", metric_logger)
-    coco_evaluator.synchronize_between_processes()
+   # metric_logger.synchronize_between_processes()
+   # print("Averaged stats:", metric_logger)
+   # coco_evaluator.synchronize_between_processes()
 
     # accumulate predictions from all images
-    coco_evaluator.accumulate()
-    coco_evaluator.summarize()
-    torch.set_num_threads(n_threads)
-    return coco_evaluator
+   # coco_evaluator.accumulate()
+   # coco_evaluator.summarize()
+   # torch.set_num_threads(n_threads)
+    return metrics
 
 
-def main(savepath=None, backbone_state_dict=None):
+def print_metrics(metrics: dict, epoch: int, thresh_list = [0.25, 0.5, 0.75, 0.9]) -> None:
+    print('[Epoch %-2.d] Evaluation results:' % (epoch + 1))
+    for thresh in thresh_list:
+        mAP = metrics[thresh]
+        print('    Threshold: %-3.3f | mAP: %-3.3f' % (thresh, mAP))
+    print('\n')
+
+
+def main(savepath, backbone_state_dict=None):
     seed = 0
     torch.manual_seed(seed)
     np.random.seed(seed)
     ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-    model = make_model(num_classes=2)
+    model = make_model(backbone_state_dict=None, num_classes=1000)
     
     device = torch.device('cpu')
     model = model.to(device)
@@ -429,9 +569,9 @@ def main(savepath=None, backbone_state_dict=None):
     weight_decay = 0.0005 # Default should be 1e-5
     optimizer = optim.SGD(model.parameters(), lr=lr, weight_decay=weight_decay)
     
-    ship_dir = '../dev/'
-    train_image_dir = os.path.join(ship_dir, 'imgs/')
-    valid_image_dir = os.path.join(ship_dir, 'imgs/')
+    ship_dir = '../dev/imgs/'
+    train_image_dir = ship_dir
+    valid_image_dir = ship_dir
     masks = get_masks(ship_dir, train_image_dir, valid_image_dir)
     image_names, filtered_masks = filter_masks(masks)
     train_ids, train_masks, valid_ids, valid_masks = get_train_valid_dfs(
@@ -452,13 +592,15 @@ def main(savepath=None, backbone_state_dict=None):
     print("Train Size: %d" % len(train_ids))
     print("Valid Size: %d" % len(valid_ids))
     
-    batch_size = 64
+    batch_size = 2
     shuffle = True
+    collate_fn = lambda batch: tuple(zip(*batch))
     loader = DataLoader(
                 dataset=vessel_dataset,
                 shuffle=shuffle,
                 #num_workers = 0,
                 batch_size=batch_size,
+                collate_fn=collate_fn,
                 pin_memory=torch.cuda.is_available()
             )
 
@@ -467,28 +609,28 @@ def main(savepath=None, backbone_state_dict=None):
                 shuffle=shuffle,
                 #num_workers = 0,
                 batch_size=batch_size,
+                collate_fn=collate_fn,
                 pin_memory=torch.cuda.is_available()
             )
     
     num_epochs = 30
-    print_freq = 100
+    print_every = 1
+    thresh_list = [0.0, 0.25, 0.5, 0.75, 1.0]
 
     print('Starting Training...\n')
-    for epoch in range(num_epochs):  # loop over the dataset multiple times
-       # train_one_epoch(model, optimizer, loader, device, epoch, lr_scheduler = None, 
-       #                 print_every = 100, num_epochs = 30)
+    for epoch in range(num_epochs):  # loop over the dataset multiple times       
+        train_one_epoch(model, optimizer, loader, device, epoch, lr_scheduler = None, 
+                        batch_size=batch_size, print_every=print_every, num_epochs = num_epochs)
         print('Epoch %d completed. Running validation...\n' % (epoch + 1))
-        metrics = evaluate(model, valid_loader, device)
+        metrics = evaluate(model, valid_loader, device, thresh_list)
+        print_metrics(metrics, epoch, thresh_list)
         print('Saving Model...\n')
         #torch.save(model.state_dict(), savepath)
         print('Model Saved.\n')
     print('Finished Training.\n')
-    print('Saving Model...\n')
-    #torch.save(model.state_dict(), savepath)
-    print('Done.')
 
 
 if __name__ == '__main__':
     #backbone_loadpath = r'../data/vessel_classifier_state_dict.pth'
     #savepath = r'vessel_detector_state_dict.pth'
-    main()
+    main(savepath=None, backbone_state_dict=None)
